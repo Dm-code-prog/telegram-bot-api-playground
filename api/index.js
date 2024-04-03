06ae0dcf9e74acf265b170262d6b3f5b4dab7b7f
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-04-03 20:59:57 +0400

    Prettify the log json

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = Fastify({
 
 app.post('/api/webhook', async (req, reply) => {
     const {body} = req
-    console.log(`Received webhook: ${JSON.stringify(body)}`)
+    console.log(`Received webhook: ${JSON.stringify(body, null, 2)}`)
     reply.code(200).send({message: 'Webhook received'})
 })
 
